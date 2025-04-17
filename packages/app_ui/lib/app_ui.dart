d665d3075e3d4638b6f691f9823bbcf130f19fb5
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2025-04-17 17:15:26 +0500

    feat: Add app color constants, typography, and theme structure
    - Introduced AppColors class with color constants for Light and Dark modes.
    - Created AppFontWeight class to define font weights.
    - Developed AppTextStyle class for managing text styles with size and weight variants.
    - Implemented AppBaseTheme and AppTheme for consistent theming across the app.
    - Added initial structure for constants, extensions, spacing, and widgets.
    - Established typography exports for easier access to font weights and styles.

--- packages/app_ui/lib/app_ui.dart
+++ packages/app_ui/lib/app_ui.dart
@@ -1,5 +1,7 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/colors/app_colors.dart';
+export 'src/constants/constants.dart';
+export 'src/extensions/extensions.dart';
+export 'src/spacing/app_spacing.dart';
+export 'src/theme/app_theme.dart';
+export 'src/typography/typography.dart';
+export 'src/widgets/widgets.dart';
