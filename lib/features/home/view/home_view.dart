96f390f4ab7eab0737284b10ede699d5dd69ad3b
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-04-20 20:03:49 +0500

    feat: Refactor HomeView layout and enhance HomeMarqueeText styling

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -10,30 +10,67 @@ class HomeView extends StatelessWidget {
       slivers: [
         SliverAppBar(
           primary: true,
-          centerTitle: true,
-          // leading: Container(
-          //   margin: EdgeInsets.only(top: 10),
-          //   color: Colors.amber,
-          //   child: HomeMarqueeText(),
-          // ),
-          // leadingWidth: 300,
-          title: SizedBox(height: 10, child: HomeMarqueeText()),
-
-          // title: HomeMarqueeText(),
+          title: ColoredBox(
+            color: Colors.white,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(child: SizedBox(height: 25, child: HomeMarqueeText())),
+                SizedBox(width: 10),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  visualDensity: VisualDensity.compact,
+                  onPressed: () {},
+                  icon: Icon(Icons.chevron_right_outlined),
+                ),
+              ],
+            ),
+          ),
           floating: true,
           pinned: true,
           actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.notifications_none)),
             IconButton(
+              padding: EdgeInsets.zero,
+              visualDensity: VisualDensity.compact,
+              onPressed: () {},
+              icon: Icon(Icons.notifications_none),
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              visualDensity: VisualDensity.compact,
               onPressed: () {},
               icon: Icon(Icons.bookmark_border_outlined),
             ),
           ],
+          forceMaterialTransparency: true,
           snap: true,
-          // bottom: PreferredSize(
-          //   preferredSize: const Size.fromHeight(48),
-          //   child: TextFormField(initialValue: 'Value'),
-          // ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(56),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              child: SearchAnchor.bar(
+                barLeading: Icon(Icons.search),
+                constraints: BoxConstraints(minHeight: 44),
+                barElevation: WidgetStatePropertyAll(0),
+                barShape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                barTrailing: [
+                  VerticalDivider(color: Colors.grey, width: 1),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.qr_code_scanner_outlined),
+                  ),
+                ],
+                barSide: WidgetStatePropertyAll(BorderSide(color: Colors.grey)),
+                suggestionsBuilder: (context, controller) {
+                  return [];
+                },
+              ),
+            ),
+          ),
         ),
         SliverList.builder(
           itemBuilder:
--- lib/features/home/widgets/home_marquee_text.dart
+++ lib/features/home/widgets/home_marquee_text.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
 
 class HomeMarqueeText extends StatelessWidget {
@@ -9,12 +9,14 @@ class HomeMarqueeText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Marquee(
       text: placemarkText ?? 'Some sample text that takes some space.',
-      style: TextStyle(fontWeight: FontWeight.bold),
+      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black38),
       scrollAxis: Axis.horizontal,
-      crossAxisAlignment: CrossAxisAlignment.start,
+
+      crossAxisAlignment: CrossAxisAlignment.center,
       blankSpace: 20.0,
       velocity: 100.0,
       pauseAfterRound: Duration(seconds: 1),
+
       startPadding: 10.0,
       accelerationDuration: Duration(seconds: 1),
       accelerationCurve: Curves.linear,
