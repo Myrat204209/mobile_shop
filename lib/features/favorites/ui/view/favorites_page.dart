3953550991a81e7663084f2c8e3b65300d39fce8
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-04-21 14:56:42 +0500

    feat: Add @RoutePage annotation to FavoritesPage and NotificationsPage

--- lib/features/favorites/ui/view/favorites_page.dart
+++ lib/features/favorites/ui/view/favorites_page.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_shop/features/favorites/favorites.dart';
 
+@RoutePage()
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
 
--- lib/features/notifications/ui/view/notifications_page.dart
+++ lib/features/notifications/ui/view/notifications_page.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_shop/features/notifications/notifications.dart';
 
+@RoutePage()
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({super.key});
 
