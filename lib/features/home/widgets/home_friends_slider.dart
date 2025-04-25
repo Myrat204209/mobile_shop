acf849e0a081bc87823aa1ed2cfdfcf0bac11f0b
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-04-25 16:16:36 +0500

    feat: Update HomeFriendsSlider layout and styling; adjust mainAccent color in AppColors; add auto_size_text dependency

--- lib/features/home/widgets/home_friends_slider.dart
+++ lib/features/home/widgets/home_friends_slider.dart
@@ -16,15 +16,15 @@ class HomeFriendsSlider extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ListTile(
-              leading: Text(
-                'Клуб Друзей Дикси',
-                style: AppTextStyle.text().titleBold(),
+              title: Text(
+                'Клуб Друзей',
+                style: AppTextStyle.display().pageTitleBold(),
               ),
-              contentPadding: EdgeInsets.zero,
+              contentPadding: EdgeInsets.symmetric(horizontal: 4),
               onTap: () {
                 // context.pushRoute(routes.friendsRoute());
               },
-              trailing: Icon(CupertinoIcons.chevron_right),
+              trailing: Icon(CupertinoIcons.arrow_right),
             ),
             SizedBox(
               height: 140,
@@ -34,15 +34,61 @@ class HomeFriendsSlider extends StatelessWidget {
                 itemBuilder: (context, index) {
                   if (index == 0) {
                     return Container(
-                      width: 200,
+                      width: 230,
                       height: 120,
+                      padding: EdgeInsets.all(8),
                       margin: EdgeInsets.only(right: 10),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainAccent,
                       ),
-                      alignment: Alignment.center,
-                      child: Text('Index 0'),
+                      alignment: Alignment.topLeft,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Друг',
+                                style: AppTextStyle.text()
+                                    .lg()
+                                    .semiBold()
+                                    .withColor(Colors.white),
+                              ),
+                              Text(
+                                '1.5%',
+                                style: AppTextStyle.text()
+                                    .lg()
+                                    .semiBold()
+                                    .withColor(Colors.white),
+                              ),
+                              FittedBox(
+                                child: Text(
+                                  'Ещё 4 дня покупок до 1% в следующем месяце',
+                                  softWrap: true,
+                                  overflow: TextOverflow.clip,
+                                  style: AppTextStyle.text()
+                                      .sm()
+                                      .semiBold()
+                                      .withColor(Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Flexible(
+                            child: SizedBox.square(
+                              dimension: 60,
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     );
                   }
                   return Container(
