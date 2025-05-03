124bec47cbb2823d64b9d49227c8d2a61d3687c3
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-03 10:06:13 +0500

    feat: Add bloc_lint and hive_ce dependencies; update preloader and desired JSON structures; implement preloader_api_client

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:data_provider/data_provider.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:mobile_shop/features/features.dart';
 import 'package:mobile_shop/main/bootstrap/bootstrap.dart';
 import 'package:talker_flutter/talker_flutter.dart';
@@ -12,7 +16,13 @@ Future<void> main() async {
   final connectivity = Connectivity();
   getIt.registerSingleton<Talker>(talker);
   getIt.registerSingleton<AppRouter>(router);
-  bootStrap(talker, () {
+  bootStrap(talker, () async {
+    const defaultBaseUrl = 'http://BASEURL';
+    final httpClient = Http(defaultBaseUrl: defaultBaseUrl);
+    HttpOverrides.global = MyHttpOverrides();
+
+    await Hive.initFlutter();
+    // Hive.registerAdapters();
     return App(connectivity: connectivity);
   });
 }
--- packages/data_provider/lib/client/client.dart
+++ packages/data_provider/lib/client/client.dart
@@ -1,1 +1,2 @@
 export 'product_api_client.dart';
+export 'preloader_api_client.dart';
