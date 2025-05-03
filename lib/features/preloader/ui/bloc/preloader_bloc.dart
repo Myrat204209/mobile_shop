bcac248df9f76b5ff3fb3441e3c2fb1b293d2aa0
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-03 14:52:03 +0500

    feat: Update PreloaderBloc to use HydratedBloc for state persistence and configure storage in main.dart

--- lib/features/preloader/ui/bloc/preloader_bloc.dart
+++ lib/features/preloader/ui/bloc/preloader_bloc.dart
@@ -2,13 +2,13 @@ import 'dart:async';
 
 import 'package:data_provider/data_provider.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mobile_shop/features/preloader/data/repository/preloader_repository.dart';
 
 part 'preloader_event.dart';
 part 'preloader_state.dart';
 
-class PreloaderBloc extends Bloc<PreloaderEvent, PreloaderState> {
+class PreloaderBloc extends HydratedBloc<PreloaderEvent, PreloaderState> {
   PreloaderBloc({required PreloaderRepository preloaderRepository})
     : _preloaderRepository = preloaderRepository,
       super(PreloaderState.initial()) {
@@ -33,5 +33,29 @@ class PreloaderBloc extends Bloc<PreloaderEvent, PreloaderState> {
       emit(state.copyWith(status: PreloaderStatus.failure));
       addError(error, stackTrace);
     }
+  }
+
+  @override
+  PreloaderState? fromJson(Map<String, dynamic> json) {
+    
+    final status = json['status'] as String?;
+    final preloadItem = json['preloadItem'] as Map<String, dynamic>?;
+    if (status == null || preloadItem == null) {
+      return null;
+    }
+    return PreloaderState(
+      status: PreloaderStatus.values.firstWhere(
+        (element) => element.toString() == status,
+      ),
+      preloadItem: PreloadItem.fromJson(preloadItem),
+    );
+  }
+
+  @override
+  Map<String, dynamic>? toJson(PreloaderState state) {
+    return {
+      'status': state.status.toString(),
+      'preloadItem': state.preloadItem?.toJson(),
+    };
   }
 }
--- lib/main/main.dart
+++ lib/main/main.dart
@@ -4,8 +4,10 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:data_provider/data_provider.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mobile_shop/features/features.dart';
 import 'package:mobile_shop/main/bootstrap/bootstrap.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 final getIt = GetIt.instance;
@@ -22,6 +24,12 @@ Future<void> main() async {
 
     final preloaderRepository = PreloaderRepository(
       preloaderClient: PreloaderClient(httpClient),
+    );
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: HydratedStorageDirectory(
+        (await getApplicationDocumentsDirectory()).path,
+      ),
+      
     );
 
     HttpOverrides.global = MyHttpOverrides();
