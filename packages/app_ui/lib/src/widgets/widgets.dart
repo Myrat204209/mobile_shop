585b5d02f0cd947436a9681b565c00c2406f64fb
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-07 16:34:40 +0500

    Refactor app_ui package by removing deprecated widgets and introducing new preloader components
    - Deleted unused widgets: AppMyFavorites, AppNewArrivalsList, AppRecommendedList, AppSearchBar, AppSlider, AppStatusBar.
    - Introduced new preloader widgets: AppBanner, AppBestseller, AppCarousel, AppCategoryGrid, AppClubCard, AppMyFavorites, AppNewArrivalsList, AppRecommendedList, AppSearchBar, AppSlider, AppStatusBar.
    - Updated widgets.dart to export new preloader components.
    - Updated pubspec.yaml to include new dependencies.

--- packages/app_ui/lib/src/widgets/widgets.dart
+++ packages/app_ui/lib/src/widgets/widgets.dart
@@ -1,11 +1,1 @@
-export 'app_status_bar.dart';
-export 'app_search_bar.dart';
-export 'app_club_card.dart';
-export 'app_banner.dart';
-export 'app_carousel.dart';
-export 'app_slider.dart';
-export 'app_category_grid.dart';
-export 'app_new_arrivals_list.dart';
-export 'app_recommended_list.dart';
-export 'app_bestseller.dart';
-export 'app_my_favorites.dart';
+export 'preloader/preloader.dart';
