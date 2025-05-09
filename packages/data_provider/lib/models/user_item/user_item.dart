0a512cacf031861a735a1c44fdd2ecfec50b1056
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-09 17:38:21 +0500

    feat: Implement UserItem model with JSON serialization and necessary annotations

--- packages/data_provider/lib/models/user_item/user_item.dart
+++ packages/data_provider/lib/models/user_item/user_item.dart
@@ -1,1 +1,33 @@
-// TODO Implement this library.
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_item.g.dart';
+
+@JsonSerializable()
+class UserItem {
+  final int id;
+  final String username;
+  @JsonKey(name: 'first_name')
+  final String? firstName;
+  @JsonKey(name: 'last_name')
+  final String? lastName;
+  final int phone;
+  final int otp;
+  @JsonKey(name: 'role_id')
+  final int roleId;
+  final String token;
+  UserItem({
+    required this.id,
+    required this.username,
+    this.firstName,
+    this.lastName,
+    required this.phone,
+    required this.otp,
+    required this.roleId,
+    required this.token,
+  });
+
+  factory UserItem.fromJson(Map<String, dynamic> json) =>
+      _$UserItemFromJson(json);
+  Map<String, dynamic> toJson() => _$UserItemToJson(this);
+}
