babc32cf3eb6ef0d02f3ee69c98f0625f9da6d63
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-12 17:08:24 +0500

    feat: Update Kotlin plugin version and enhance ProfileView layout with new UI components

--- lib/features/profile/view/profile_view.dart
+++ lib/features/profile/view/profile_view.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:app_ui/app_ui.dart';
+import 'package:flutter/cupertino.dart' show CupertinoIcons;
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -5,6 +9,104 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile Page'));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Icon(Icons.store_rounded, size: 30, color: AppColors.mainAccent),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {},
+                child: AppBarMarqueeText(placemarkText: ' sdsdfdsf'),
+              ),
+            ),
+            AppActionIcon(
+              icon: CupertinoIcons.chevron_right,
+              onTap: () {},
+              size: 20,
+            ),
+          ],
+        ),
+
+        ListTile(
+          leading: CircleAvatar(
+            radius: 25,
+            child: Icon(Icons.apple, size: 45).centralize(),
+          ),
+          title: Text(
+            'Имя пользователя',
+            style: AppTextStyle.text().lg().bold(),
+          ),
+          contentPadding: EdgeInsets.zero,
+          subtitle: GestureDetector(
+            onTap: () {
+              log('Printed..');
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Личные данные',
+                  style: AppTextStyle.text().sm().medium().withColor(
+                    AppColors.textLight,
+                  ),
+                ),
+                Icon(
+                  CupertinoIcons.chevron_right,
+                  size: 16,
+                  color: AppColors.iconColor,
+                ).paddingOnly(left: 5),
+              ],
+            ),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.chat_outlined)),
+            ],
+          ),
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: AppColors.mainAccent,
+            borderRadius: kCircular15Border,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Drug',
+                    style: AppTextStyle.text().md().bold().withColor(
+                      AppColors.bgMain,
+                    ),
+                  ),
+                  Text(
+                    '1.5%',
+                    style: AppTextStyle.text().md().bold().withColor(
+                      AppColors.bgMain,
+                    ),
+                  ),
+                  Text('Drug'),
+                ],
+              ).paddingSymmetric(horizontal: 10),
+
+              SizedBox.square(
+                dimension: 80,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: kCircular10Border,
+                    color: AppColors.bgMain,
+                  ),
+                ),
+              ).paddingAll(5),
+            ],
+          ),
+        ),
+      ],
+    ).paddingAll(15);
   }
 }
