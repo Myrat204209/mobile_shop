738d99cde8f68f898f4476800df6bc943e5d7088
Myratgeldi Annamuhammedow <138607217+Myrat204209@users.noreply.github.com>
2025-05-19 10:10:29 +0500

    feat: Enhance BeveragesView with product details navigation and update AppStickyHeaderList to support product selection

--- lib/features/beverages/ui/view/beverages_view.dart
+++ lib/features/beverages/ui/view/beverages_view.dart
@@ -1,12 +1,17 @@
-
 import 'package:app_ui/app_ui.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_shop/features/features.dart';
 
 class BeveragesView extends StatelessWidget {
   const BeveragesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppStickyHeaderList();
+    return AppStickyHeaderList(
+      onPressed: () {
+        context.pushRoute(ProductDetailsRoute());
+      },
+    );
   }
 }
--- packages/app_ui/lib/src/widgets/app_sticky_header_list.dart
+++ packages/app_ui/lib/src/widgets/app_sticky_header_list.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
@@ -76,8 +77,8 @@ final List<ProductCategory> allCategories = [
 
 class AppStickyHeaderList extends StatelessWidget {
   final List<ProductCategory> categories = allCategories;
-
-  AppStickyHeaderList({super.key});
+  AppStickyHeaderList({super.key, required this.onPressed});
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -89,11 +90,8 @@ class AppStickyHeaderList extends StatelessWidget {
             pinned: true,
             floating: false,
             actions: [IconButton(icon: Icon(Icons.search), onPressed: null)],
-            // elevation: 1.0, // Optional elevation
-            // backgroundColor: Colors.white, // Or your theme's app bar color
           ),
 
-          // 2. Iterate over your categories to create sticky header sections
           for (final category in categories)
             SliverStickyHeader(
               // sticky: true,
@@ -130,7 +128,8 @@ class AppStickyHeaderList extends StatelessWidget {
                     int index,
                   ) {
                     final product = category.products[index];
-                    return _buildProductCard(context, product);
+                    return ProductDetailsCard(onProductTapped: onPressed, product: product);
+
                   }, childCount: category.products.length),
                 ),
               ),
@@ -140,66 +139,81 @@ class AppStickyHeaderList extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildProductCard(BuildContext context, Product product) {
+class ProductDetailsCard extends StatelessWidget {
+  const ProductDetailsCard({
+    super.key,
+    required this.product,
+    required this.onProductTapped,
+  });
+
+  final Product product;
+  final VoidCallback onProductTapped;
+  @override
+  Widget build(BuildContext context) {
     return Card(
       elevation: 1.0,
+
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
       clipBehavior: Clip.antiAlias,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(flex: 3, child: FlutterLogo()),
-          Expanded(
-            flex: 2, // Less space for text details
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment:
-                    MainAxisAlignment.spaceBetween, // Distribute space
-                children: <Widget>[
-                  Text(
-                    product.name,
-                    style: const TextStyle(
-                      fontSize: 13.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.price,
-                        style: const TextStyle(
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepOrange,
-                        ),
+      child: InkWell(
+        onTap: onProductTapped,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(flex: 3, child: FlutterLogo()),
+            Expanded(
+              flex: 2, // Less space for text details
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment:
+                      MainAxisAlignment.spaceBetween, // Distribute space
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: const TextStyle(
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.w500,
                       ),
-                      // Your "+" button
-                      SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: IconButton.filled(
-                          padding: EdgeInsets.zero,
-                          iconSize: 18,
-                          icon: Icon(Icons.add),
-                          onPressed: () {},
-                          style: IconButton.styleFrom(
-                            backgroundColor: Colors.orange,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.price,
+                          style: const TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepOrange,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        // Your "+" button
+                        SizedBox(
+                          width: 30,
+                          height: 30,
+                          child: IconButton.filled(
+                            padding: EdgeInsets.zero,
+                            iconSize: 18,
+                            icon: Icon(Icons.add),
+                            onPressed: () {},
+                            style: IconButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
