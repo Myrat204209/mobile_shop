5f740c606f69080cba27f97376e33be7c0ce73be
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2025-05-23 17:53:12 +0500

    feat: Add GeolocationInitResult class to manage location initialization results

--- lib/core/location/location_service.dart
+++ lib/core/location/location_service.dart
@@ -9,6 +9,29 @@ enum LocationPermissionStatus {
   unknown,
 }
 
+class GeolocationInitResult {
+  final LocationService service;
+  final LocationPermissionStatus permissionStatus;
+  final Position? initialPosition;
+  final bool locationServicesEnabled;
+
+  GeolocationInitResult({
+    required this.service,
+    required this.locationServicesEnabled,
+    required this.permissionStatus,
+    this.initialPosition,
+  });
+
+  bool get canProceedWithLocation =>
+      locationServicesEnabled &&
+      permissionStatus == LocationPermissionStatus.granted;
+
+  @override
+  String toString() {
+    return 'GeolocationInitResult(service: $service, locationServicesEnabled: $locationServicesEnabled, permissionStatus: $permissionStatus, initialPosition: $initialPosition)';
+  }
+}
+
 class LocationService {
   final GeolocatorPlatform _geolocatorPlatform;
   final Talker? _talker;
@@ -18,4 +41,30 @@ class LocationService {
     Talker? talker,
   }) : _geolocatorPlatform = geolocatorPlatform ?? GeolocatorPlatform.instance,
        _talker = talker;
+
+  static Future<GeolocationInitResult?> init({
+    Talker? talker,
+    GeolocatorPlatform? geolocatorPlatform,
+
+    bool fetchInitialPosition = true,
+    LocationAccuracy initialAccuracy = LocationAccuracy.medium,
+    Duration? timeLimit,
+  }) async {
+    talker?.info('GeolocationService: Initializing...');
+    final service = LocationService._internal(
+      geolocatorPlatform: geolocatorPlatform,
+      talker: talker,
+    );
+    return null;
+    //  bool servicesEnabled = await service.isLocationServiceEnabled();
+    // if (!servicesEnabled) {
+    //   talker?.warning('GeolocationService: Location services are disabled.');
+    //   return GeolocationInitResult(
+    //     service: service,
+    //     locationServicesEnabled: false,
+    //     permissionStatus: LocationPermissionStatus.serviceDisabled,
+    //     initialPosition: null,
+    //   );
+    // }
+  }
 }
